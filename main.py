9e3b219a6f0c6ecab40fe58d6068ad3aca00e968
Andres-Neira <andres.neira.pa@gmail.com>
2025-02-10 21:54:40 -0500

    Añadir funciones agregar cliente y agregar mascota
    agregarCliente(self): añade el cliente y evalua que todos los datos se registren, luego se añaden a la lista clientes
    agregarMascota(self): busca el cliente , aplica vectores busca entre la lista self.clentes el nombre que coincida con cliente, luego registra la mascota en la lista de cliente.agregarMascota()

--- main.py
+++ main.py
@@ -1,29 +1,87 @@
-class Persona:
-    def __init__(self, nombre, edad):
+from abc import ABC, abstractmethod
+class Persona(ABC):
+    def __init__(self, nombre, edad, contacto, direccion,):
         self.nombre = nombre
         self.edad = edad
+        self.contacto=contacto
+        self.direccion=direccion
+
+    @abstractmethod
+    def mostrardatos(self):
+        pass
+
 class Cliente(Persona):
-    def __init__(self, nombre, edad, contacto, direccion):
-        super().__init__(nombre, edad)
-        self.contacto = contacto
-        self.direccion= direccion
-        clientes = []
+    def __init__(self, nombre, edad,contacto,direccion):
+        super().__init__(nombre, edad,contacto,direccion)
+        self.mascota=[]
+
+    def agregarMascota(self,mascota):
+        self.mascota.append(mascota)
+
+    def mostrardatos(self):
+        return f'El cliente se registro con exito !'
+
+
 class Veterinario(Persona):
     def __init__(self, nombre, edad):
         super().__init__(nombre, edad)
-        Veterinarios=[]
-
+        veterinarios = []
 class Mascota:
-    def __init__(self, nombre, especie, edad, raza, historial_citas,cliente):
+    def __init__(self, nombre, especie, edad, raza, historial_citas):
         self.nombre = nombre
         self.especie = especie
         self.edad = edad
         self.raza = raza
-        self.historial_citas = historial_citas
+        self.historial_citas =[]
+
+
+class Cita:
+    def __init__(self, fecha , hora, servicio,veterinario,cliente,mascota ):
+        self.name = fecha
+        self.hora = hora
+        self.servicio= servicio
+        self.veterinario = veterinario
         self.cliente = cliente
+        self.mascota = mascota
+
+class sistema:
+    def __init__(self):
+        self.clientes=[]
+    def agregarCliente(self):
+        try:
+            nombre=input('Digite el nombre del cliente').strip()
+            edad=input('Digite el nombre del cliente').strip()
+            contacto=input('Digite el numero de contacto del cliente').strip()
+            direccion=input('Digite la dirección de domicilio del cliente').strip()
+            if not nombre or not edad or not contacto or not direccion:
+                raise ValueError('Todos los datos se debe de registrar')
+
+            cliente=Cliente(nombre,edad,contacto,direccion)
+            self.clientes.append(cliente)
+            return f'Cliente registrado con exito'
+        except ValueError as e:
+            print(f'error : {e}')
+    def agregarMascota(self):
+        try:
+            nombre_cliente=input('Digite el nombre del cliete').strip()
+            cliente=next((c for c in self.clientes if c.nombre == nombre_cliente),None)
+            if not cliente:
+                raise ValueError('Cliente no encontrado')
+            nombre=input('Digite el nombre de la mascota')
+            especie=input('Digite la especie de la mascota')
+            edad=input('Digite la edad de la mascota')
+            raza=input('Digite la raza de la mascota')
+            if not nombre or not especie or not edad or not raza:
+                raise ValueError('Todos los datos se deben de registrar')
+            mascota=Mascota(nombre,especie,edad,raza)
+            cliente.agregarMascota(mascota)
+        except ValueError as e:
+            print(f'Error : {e}')
+
+sistema()
 
 
-
+'''
 def menu():
     while True:
         print('SISTEMA DE GESTION ADMINISTRATIVA VETERINARIA DEVSENIOR\n'
@@ -39,7 +97,6 @@ def menu():
                 '9 Salir')
         opcion=input('Elija una opción (Ejemplo : 1) : ')
         if opcion == "1":
-            pass
         elif opcion == '2':
             pass
         elif opcion == '3':
@@ -59,4 +116,15 @@ def menu():
             break
         else:
             print(' Opcion invalida Elija alguna de las opciones anteriores')
-menu()
+menu()'''
+'''
+
+cliente1=Cliente("Andres","14","3144572234","cr 13 #18-90","Chocolight")
+print(cliente1.nombre)
+print(cliente1.edad)
+print(cliente1.contacto)
+print(cliente1.direccion)
+print(cliente1.mascota)
+veterinario1=Veterinario("Juan","12")
+print(veterinario1.nombre)
+print(veterinario1.edad)'''
